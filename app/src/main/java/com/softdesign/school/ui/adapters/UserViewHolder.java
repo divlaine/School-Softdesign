3b9cab2ee074eaf193f037f2f15879627d1f5c0d
divlaine <divlaine@gmail.com>
2016-02-23 22:49:14 +0400

    home_task 5 butterknife added

--- app/src/main/java/com/softdesign/school/ui/adapters/UserViewHolder.java
+++ app/src/main/java/com/softdesign/school/ui/adapters/UserViewHolder.java
@@ -7,14 +7,16 @@ import android.widget.TextView;
 
 import com.softdesign.school.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class UserViewHolder extends RecyclerView.ViewHolder {
 
-    protected TextView name;
-    protected ImageView image;
+    @Bind(R.id.fragment_contacts_name) TextView name;
+    @Bind(R.id.fragment_contacts_icon) ImageView image;
 
     public UserViewHolder(View itemView) {
         super(itemView);
-        name = (TextView) itemView.findViewById(R.id.fragment_contacts_name);
-        image = (ImageView) itemView.findViewById(R.id.fragment_contacts_icon);
+        ButterKnife.bind(this, itemView);
     }
 }
